7c53df1cd4c21eaaef72c6693927d97464003e13
sfc-gh-nrajaofetra <94199571+sfc-gh-nrajaofetra@users.noreply.github.com>
2022-10-13 00:16:26 +0200

    commit v2

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "snowflake_database" "demo_db_tf" {
 }
 
 resource "snowflake_schema" "demo_schema" {
-  database = snowflake_database.demo_db.name
+  database = snowflake_database.demo_db_tf.name
   name     = "DEMO_SCHEMA_TF"
   comment  = "Schema for Snowflake Terraform demo"
 }
